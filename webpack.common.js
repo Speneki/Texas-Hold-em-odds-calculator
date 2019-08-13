b1b3a8063dfeb9c2108b411556fe389255b92fbc
Spencer Tassone <spencerwilliamtassone@gmail.com>
2019-08-13 02:47:35 -0400

    bug fixing

--- webpack.common.js
+++ webpack.common.js
@@ -3,17 +3,24 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const outputDir = "./dist";
 
 module.exports = {
-    entry: path.resolve(__dirname, "src", "index.jsx"),
+    entry: path.resolve(__dirname, "src", "index.js"), //
     output: {
         path: path.join(__dirname, outputDir),
-        filename: "[name].js",
+        filename: "pokerOdds.js",
         publicPath: "/dist/"
     },
     resolve: {
-        extensions: [".js", ".jsx"]
+        extensions: [".js"] // if we were using React.js, we would include ".jsx"
     },
     module: {
         rules: [
+            {
+                test: /\.js$/, // if we were using React.js, we would use \.jsx?$/
+                use: {
+                    loader: "babel-loader",
+                    options: { presets: ["env"] } // if we were using React.js, we would include "react"
+                }
+            },
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
