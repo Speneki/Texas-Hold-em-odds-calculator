f293451b9023581577f75f4de9034318bee81b48
Spencer Tassone <spencerwilliamtassone@gmail.com>
2019-08-13 18:03:40 -0400

    figured out clickling on each square

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+import * as PokerOdds from "poker-odds";
+
 let board = d3.select("#board") 
     .append("svg")
     .attr("height", 1000)
@@ -12,32 +15,35 @@ let table = board.append("ellipse")
     .style("stroke", "black")
     .style("stroke-width", 30);
 
-// let defs = images.append("defs")
-//     .attr("id", "cardback")
-//     .attr("height", "100%")
-//     .attr("width", "100%")
-//     .attr("patternContentUnits", "objectBoundingBox")
-//     .append("image")
-//     // .attr("id", "cardback")
-//     .attr("height", 1)
-//     .attr("width", 1)
-//     .attr("preserveAspectRatio", "none")
-//     .attr("xmlns:xlink", "https://www.ezmagicrobbins.com/images/B11649-1.jpg")
-//     .attr("xlink:href", "https://www.ezmagicrobbins.com/images/B11649-1.jpg");
+
+// let img_url = function (d) { return "url(#img_" + d + ")"; }
+
+// let defs = board.append("defs")
+//     .append("pattern")
+//     .attr("id", "img_cardback")
+//     .attr("height", 150)
+//     .attr("width", 100)
+    // .attr("patternContentUnits", "objectBoundingBox")
+    // .attr("overflow", "visable")
+    // .append("image")
+    // .attr("id", "cardback")
+    // .attr("height", 100)
+    // .attr("width", 100)
+    // .attr("x", 0)
+    // .attr("y", 0)
+    // .attr("preserveAspectRatio", "none")
+    // .attr("xmlns:xlink", "https://www.ezmagicrobbins.com/images/B11649-1.jpg")
+    // .attr("xlink:href", "http://lorempixel.com/150/150/people/3");
 
 let hero1 = board.append("rect")
     .attr("x", 150)
     .attr("y", 30)
     .attr("width", 100)
     .attr("height", 150)
-    .attr("fill", "url(#cardback)")
+    .style("fill", "white")
     .style("stroke", "black")
 
-hero1.append('svg:image')
-    .attr('xlink:href', 'https://www.ezmagicrobbins.com/images/B11649-1.jpg')
-    .attr("height", 150)
-    .attr("width", 100)
-    
+
 let hero2 = board.append("rect")
     .attr("x", 260)
     .attr("y", 30)
@@ -111,3 +117,35 @@ const cards = [
     "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "Th", "Jh", "Qh", "Kh", "Ah",
     "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "Ts", "Js", "Qs", "Ks", "As",
 ];
+
+let heroHand = [null, null]
+let villanHand = [null, null]
+
+hero1.on("click", function() {
+    heroHand[0] = cards[Math.floor(Math.random() * cards.length)]
+    if (!heroHand.includes(null) && !villanHand.includes(null)) {
+        console.log('yup')
+    }
+})
+
+hero2.on("click", function() {
+    heroHand[1] = cards[Math.floor(Math.random() * cards.length)]
+    if (!heroHand.includes(null) && !villanHand.includes(null)) {
+        console.log('yup')
+    }
+})
+
+
+villan1.on("click", function() {
+    villanHand[0] = cards[Math.floor(Math.random() * cards.length)]
+    if (!heroHand.includes(null) && !villanHand.includes(null)) {
+        console.log('yup')
+    }
+})
+
+villan2.on("click", function() {
+    villanHand[1] = cards[Math.floor(Math.random() * cards.length)]
+    if (!heroHand.includes(null) && !villanHand.includes(null)) {
+        console.log(calculateEquity([heroHand, villanHand]))
+    }
+})
